8ad4c217278467ea9e967ebf69b2817f507ecfb8
Holman <holman.londono@udea.edu.co>
2021-09-24 02:51:37 -0500

    Inicio de la codificacion Inicie con el codigo del programa, no avance mucho en el codigo ya que estaba buscando ejemplos de la funcion load y analizando la mejor forma para almacenar los datos de la matriz de la imagen

--- Desarrollo/Codigo fuente/Parcial_2_QT/main.cpp
+++ Desarrollo/Codigo fuente/Parcial_2_QT/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <QImage>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    QImage Imagen;
+    string FilePath = "../Parcial_2_QT/Imagenes/";
+    string FileName;
+
+    cout << "Ingrese el nombre de la imagen a tratar: "; cin >> FileName; cout << endl;
+    FilePath.append(FileName);
+    if(Imagen.load(FilePath.c_str())){
+        cout << "Imagen tratada con exito. " << endl;
+    }
+    else{
+        cout << "No se encontro la imagen. " << endl;
+        cout << "Recuerde que la imagen debe estar guardada dentro de la carpeta del programa. " << endl;
+    }
+
     return 0;
 }
