e2f2aeab60390707768d558c9c9d9449f48c268a
Holman <holman.londono@udea.edu.co>
2021-09-24 23:28:59 -0500

    Creacion de Matriz para datos de imagen Cree una matriz para almacenar los valores RGB de la imagen, esta la hice con memoria dinamica ya que el tamaño de la imagen que ingresan no tiene un valor fijo sino que varia segun la imagen, ademas corregi un fallo que tenia con el cin y lo reemplace por un getline debido a que si me ingresaban el nombre de una imagen que tuviera un espacio en blanco no se iba a almacenar el nombre completo.

--- Desarrollo/Codigo fuente/Parcial_2_QT/main.cpp
+++ Desarrollo/Codigo fuente/Parcial_2_QT/main.cpp
@@ -6,17 +6,27 @@ using namespace std;
 int main()
 {
     QImage Imagen;
+    int **MatrizImg;
+    int Ancho, Alto;
+    unsigned long Dimension;
     string FilePath = "../Parcial_2_QT/Imagenes/";
     string FileName;
 
-    cout << "Ingrese el nombre de la imagen a tratar: "; cin >> FileName; cout << endl;
+    cout << "Ingrese el nombre de la imagen a tratar: "; getline(cin, FileName); cout << endl;
     FilePath.append(FileName);
     if(Imagen.load(FilePath.c_str())){
         cout << "Imagen tratada con exito. " << endl;
+        Ancho = Imagen.width();
+        Alto = Imagen.height();
+        Dimension = Ancho*Alto;
+        MatrizImg = new int*[Dimension];
+        for(unsigned long FilaM=0; FilaM<Dimension; FilaM++){
+            MatrizImg[FilaM] = new int[3];
+        }
     }
     else{
         cout << "No se encontro la imagen. " << endl;
-        cout << "Recuerde que la imagen debe estar guardada dentro de la carpeta del programa. " << endl;
+        cout << "Recuerde que la imagen debe estar guardada dentro de la carpeta del programa, llamada \"Imagenes\". " << endl;
     }
 
     return 0;
