ddbe4527bdc175a911cdd1afbd7cb998e919e818
Holman <holman.londono@udea.edu.co>
2021-09-25 23:53:25 -0500

    Datos de imagen guardados en matriz. Hice un doble ciclo for para almacenar todos los datos RGB de la imagen a tratar dentro de una matriz para asi poder tratar estos datos y hacer el sub o sobremuestreo de la imagen ingresada.

--- Desarrollo/Codigo fuente/Parcial_2_QT/main.cpp
+++ Desarrollo/Codigo fuente/Parcial_2_QT/main.cpp
@@ -8,7 +8,7 @@ int main()
     QImage Imagen;
     int **MatrizImg;
     int Ancho, Alto;
-    unsigned long Dimension;
+    unsigned long Dimension, PosPixel = 0;
     string FilePath = "../Parcial_2_QT/Imagenes/";
     string FileName;
 
@@ -22,6 +22,15 @@ int main()
         MatrizImg = new int*[Dimension];
         for(unsigned long FilaM=0; FilaM<Dimension; FilaM++){
             MatrizImg[FilaM] = new int[3];
+        }
+
+        for(int PosY=0; PosY<Imagen.height(); PosY++){
+            for(int PosX=0; PosX<Imagen.height(); PosX++){
+                MatrizImg[PosPixel][0] = Imagen.pixelColor(PosX, PosY).red();
+                MatrizImg[PosPixel][1] = Imagen.pixelColor(PosX, PosY).green();
+                MatrizImg[PosPixel][2] = Imagen.pixelColor(PosX, PosY).blue();
+                PosPixel++;
+            }
         }
     }
     else{
